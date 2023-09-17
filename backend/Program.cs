79216411212988ab90202e1fb164f9aec438f49e
Serhan miah <101991709+SerhanMiah@users.noreply.github.com>
2023-09-17 18:33:44 +0100

    Allow origins weblink in appsettings redo again

--- backend/Program.cs
+++ backend/Program.cs
@@ -10,28 +10,21 @@ using VideoGameAppBackend.Controllers;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+// Configuration for DbContext and Identity
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<GameDbContext>(options => 
-    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))); 
-
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<GameDbContext>()
     .AddDefaultTokenProviders();
 
+// Configuration for JWT Authentication
 string? jwtKey = configuration["Jwt:Key"];
 if (jwtKey == null)
 {
     throw new Exception("Missing JWT key in configuration");
 }
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-});
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -47,39 +40,47 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>() ?? new string[0];
-
-builder.Services.AddCors(options =>
+// JSON Serializer Options
+builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    options.AddPolicy("AllowMyOrigin",
-        builder => 
-        {
-            builder.WithOrigins(allowedOrigins.Length > 0 ? allowedOrigins : new[] { "YourDefaultOriginHere" })
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
-        });
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
 
+// Configuration for CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>() ?? new string[0];
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name:MyAllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins(allowedOrigins.Length > 0 ? allowedOrigins : new[] { "YourDefaultOriginHere" })
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .SetIsOriginAllowedToAllowWildcardSubdomains();
+    });
+});
+
+// Adding Application Part
 builder.Services.AddControllers().AddApplicationPart(typeof(ShoppingCartController).Assembly);
 
+// Build App
 var app = builder.Build();
 
+// Middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseCors("AllowMyOrigin");
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
