1bdd9681965ef3eb33446ee8ee30aff81ba733d3
S.Miah <serhanmiah@icloud.com>
2023-09-20 13:04:51 +0100

    switching from axios to http client

--- frontend/src/app/game-list/game-list.component.ts
+++ frontend/src/app/game-list/game-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
-import axios from 'axios';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environment/environment';
-
 
 interface Game {
   id: number;
@@ -20,49 +19,50 @@ interface Game {
 export class GameListComponent implements OnInit {
   games: Game[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
+
   async ngOnInit(): Promise<void> {
     try {
-      const { data } = await axios.get(`${environment.apiBaseUrl}/api/Game`);
+      const data: any = await this.http.get(`${environment.apiBaseUrl}/api/Game`).toPromise();
 
-        // Check if data exists
-        if (!data) {
-            console.error('Received no data from API.');
-            return;
-        }
+      // Check if data exists
+      if (!data) {
+          console.error('Received no data from API.');
+          return;
+      }
 
-        // Check if $values exists and is an array
-        if (!data.$values || !Array.isArray(data.$values)) {
-            console.error('Data does not contain a valid $values array:', data);
-            return;
-        }
-        console.log(data)
+      // Check if $values exists and is an array
+      if (!data.$values || !Array.isArray(data.$values)) {
+          console.error('Data does not contain a valid $values array:', data);
+          return;
+      }
+      console.log(data)
 
-        // Map the games from the $values property
-        this.games = data.$values.map((gameData: {
-          id: number;
-          title: string;
-          description: string;
-          price: number;
-          GameImage: { path: string; } 
-        }) => ({
-          id: gameData.id,
-          title: gameData.title,
-          description: gameData.description,
-          price: gameData.price,
-          coverImageUrl: gameData.GameImage?.path, 
-        }));
-        
+      // Map the games from the $values property
+      this.games = data.$values.map((gameData: {
+        id: number;
+        title: string;
+        description: string;
+        price: number;
+        GameImage: { path: string; } 
+      }) => ({
+        id: gameData.id,
+        title: gameData.title,
+        description: gameData.description,
+        price: gameData.price,
+        coverImageUrl: gameData.GameImage?.path, 
+      }));
+      
 
-        // Log the mapped games for verification
-        console.log('Mapped games:', this.games);
+      // Log the mapped games for verification
+      console.log('Mapped games:', this.games);
 
     } catch (error) {
-        console.error('There was an error fetching the games', error);
+      console.error('There was an error fetching the games', error);
     }
   }
 
-  // Updated onSelect to use the router to navigate to the game details page
+
   onSelect(game: Game): void {
     this.router.navigate(['/game', game.id]);
   }
